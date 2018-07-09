924fc93b33dd3c14ffea330dc5b6ee846882a90c
Federico Bevacqua <fede.el.xeneize@gmail.com>
2018-07-09 14:57:04 -0300

    Primeras pruebas con pagos para MercadoPago.

--- app/view/abm/pagarExpensa.php
+++ app/view/abm/pagarExpensa.php
@@ -1,8 +1,15 @@
 <?php
-require_once '../../config/Conexion.php'; 
+require_once '../../config/Conexion.php';
 session_start();
-if(!isset($_SESSION['admin'])){
-    header("Location: ../index.php");} ?>
+if(!isset($_SESSION['admin']) && !isset($_SESSION['operador']) && !isset($_SESSION['propietario'])) {
+	header("Location: ../index.php");
+}
+
+require_once "../../lib/mercadopago.php";
+
+$mp = new MP("426543208337217", "QvFA81UyiPK8pKl727ikvN43lraFFbmC");
+$mp->sandbox_mode(TRUE);
+?>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -50,6 +57,67 @@ if(!isset($_SESSION['admin'])){
 					echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Error: La expensa indicada no existe o no tiene acceso a ella.</div>';
 				} else {
 					$expensa = mysqli_fetch_assoc($sql);
+
+					// Configuracion para MercadoPago
+					$preference_data = array(
+						"items" => array(
+							array(
+								"id" => $idExpensa,
+								"title" => "Pago de Expensa",
+								"description" => "",
+								"category_id" => "services",
+								"currency_id" => "ARS",
+								"quantity" => 1,
+								"unit_price" => (float) $expensa["importe"]
+							)
+							),
+							"back_urls" => array(
+								// Si el pago fue exitoso:
+								"success" => "http://localhost/consorcio/app/view/listaReclamos.php",
+								// Si el pago falló:
+								// TODO: Redirigir a pagina de error
+								"failure" => "http://localhost/consorcio/app/view/listaReclamos.php",
+								// Si el pago esta pendiente:
+								// TODO: Redirigir a pagina de pendiente
+								"pending" => "http://localhost/consorcio/app/view/listaReclamos.php"
+							)
+							// ,
+							// // Ver de setear referencia a la expensa y al usuario
+							// "external_reference" => $idExpensa,
+					);
+					$preference = $mp->create_preference($preference_data);
+
+					?>
+
+					<a href="" onclick="pagar()" name="MP-Checkout" class="blue-ar-m-rn-arall">Pagooooo</a>
+					<!-- <a onclick="pagar()" class="blue-ar-m-rn-arall">Pagar</a> -->
+
+					<script type="text/javascript">
+
+function pagar() {
+	$MPC.openCheckout ({
+    url: "<?php echo $preference["response"]["init_point"]; ?>",
+    mode: "modal",
+    onreturn: function(json) {
+		// debugger;
+		// execute_my_onreturn (Sólo modal)
+		console.log(JSON.stringify(json));
+		if (json.collection_status=='approved'){
+        alert ('Pago acreditado');
+    } else if(json.collection_status=='pending'){
+        alert ('El usuario no completó el pago');
+    } else if(json.collection_status=='in_process'){    
+        alert ('El pago está siendo revisado');    
+    } else if(json.collection_status=='rejected'){
+        alert ('El pago fué rechazado, el usuario puede intentar nuevamente el pago');
+    } else if(json.collection_status==null){
+        alert ('El usuario no completó el proceso de pago, no se ha generado ningún pago');
+    }
+    }
+});
+}
+</script>
+				<?php
 				}
 			}
 
@@ -169,6 +237,11 @@ if(!isset($_SESSION['admin'])){
 	</div>
 
   	<div class="corte"></div>
-    <?php include('../template/footer.php'); ?>
+	<?php include('../template/footer.php'); ?>
+
+	<!-- Pega este código antes de cerrar la etiqueta </body> -->
+	<script type="text/javascript">
+	(function(){function $MPC_load(){window.$MPC_loaded !== true && (function(){var s = document.createElement("script");s.type = "text/javascript";s.async = true;s.src = document.location.protocol+"//secure.mlstatic.com/mptools/render.js";var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);window.$MPC_loaded = true;})();}window.$MPC_loaded !== true ? (window.attachEvent ?window.attachEvent('onload', $MPC_load) : window.addEventListener('load', $MPC_load, false)) : null;})();
+	</script>
     </body>
 </html>
