b80b4e397edde1dcdd35432ad7dbe8ba126929ab
Federico Bevacqua <fede.el.xeneize@gmail.com>
2018-07-10 02:08:42 -0300

    Agregado icono en NavBar.

--- app/view/template/nav.php
+++ app/view/template/nav.php
@@ -1,5 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-  <a class="navbar-brand" href="#">Menu</a>
+  <!-- <a class="navbar-brand" href="#">Menu</a> -->
+  <a class="navbar-brand" href="#">
+    <img src="/consorcio/public/img/building-icon.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+    Menu
+  </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
