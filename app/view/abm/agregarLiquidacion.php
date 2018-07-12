eaba2cc8c3fbcffebaca7f02ad8eb5d735577716
Federico Bevacqua <fede.el.xeneize@gmail.com>
2018-07-11 22:10:24 -0300

    Corregidos años de liquidacion nueva.

--- app/view/abm/agregarLiquidacion.php
+++ app/view/abm/agregarLiquidacion.php
@@ -123,9 +123,9 @@ if (!isset($_SESSION['admin']) && !isset($_SESSION['operador'])) {
 							<option value='2020'>2020</option>
 							<option value='2021'>2021</option>
 							<option value='2022'>2022</option>
-							<option value='2022'>2023</option>
-							<option value='2022'>2024</option>
-							<option value='2022'>2025</option>
+							<option value='2023'>2023</option>
+							<option value='2024'>2024</option>
+							<option value='2025'>2025</option>
 						</select>
 					</div>
 				</div>
