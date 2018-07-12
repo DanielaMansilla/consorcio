143ca1354db3d0177f00d50269f6c554796fbb4a
Federico Bevacqua <fede.el.xeneize@gmail.com>
2018-07-12 00:37:21 -0300

    Arreglado como se muestran los errores en editarConsorcio. Al editar algo, se muestran los datos actualizados (antes no pasaba).

--- app/view/abm/editarConsorcio.php
+++ app/view/abm/editarConsorcio.php
@@ -3,9 +3,10 @@ require_once '../../config/Conexion.php';
 require_once '../../clases/Proveedor.php';
 session_start();
 //Si es Admin o Operador puede visualizar la pagina.
-if(!isset($_SESSION['admin'])){
-	if(!isset($_SESSION['operador'])){ //verificar si el operador necesita editar.
-		header("Location: ../index.php");}} ?>
+if (!isset($_SESSION['admin']) && !isset($_SESSION['operador'])) {
+	header("Location: ../index.php");
+} 
+?>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -27,84 +28,77 @@ if(!isset($_SESSION['admin'])){
 			// escaping, additionally removing everything that could be (html/javascript-) code
 			$nik = mysqli_real_escape_string($conexion,(strip_tags($_GET["nik"],ENT_QUOTES)));
 			$sql = mysqli_query($conexion, "SELECT * FROM consorcio WHERE idConsorcio='$nik'");
-			if(mysqli_num_rows($sql) == 0){
+			// Si el consorcio no existe redirecciono al index
+			if (mysqli_num_rows($sql) == 0) {
 				header("Location: ../index.php");
-			}else{
+			} else {
 				$row = mysqli_fetch_assoc($sql);
 			}
-			if(isset($_POST['save'])){ 
-				$nombre		     = mysqli_real_escape_string($conexion,(strip_tags($_POST["nombre"],ENT_QUOTES)));//Escanpando caracteres 
-                $cuit		     = mysqli_real_escape_string($conexion,(strip_tags($_POST["cuit"],ENT_QUOTES)));//Escanpando caracteres 
+			if (isset($_POST['save'])) { 
+				$nombre		    = mysqli_real_escape_string($conexion,(strip_tags($_POST["nombre"],ENT_QUOTES)));//Escanpando caracteres 
+                $cuit		    = mysqli_real_escape_string($conexion,(strip_tags($_POST["cuit"],ENT_QUOTES)));//Escanpando caracteres 
                 $codigoPostal   = mysqli_real_escape_string($conexion,(strip_tags($_POST["codigoPostal"],ENT_QUOTES)));//Escanpando caracteres 
-                $telefono   = mysqli_real_escape_string($conexion,(strip_tags($_POST["telefono"],ENT_QUOTES)));//Escanpando caracteres 
-                $correo   = mysqli_real_escape_string($conexion,(strip_tags($_POST["correo"],ENT_QUOTES)));//Escanpando caracteres 
-				$direccion   = mysqli_real_escape_string($conexion,(strip_tags($_POST["direccion"],ENT_QUOTES)));//Escanpando caracteres 
+                $telefono   	= mysqli_real_escape_string($conexion,(strip_tags($_POST["telefono"],ENT_QUOTES)));//Escanpando caracteres 
+                $correo   		= mysqli_real_escape_string($conexion,(strip_tags($_POST["correo"],ENT_QUOTES)));//Escanpando caracteres 
+				$direccion  	= mysqli_real_escape_string($conexion,(strip_tags($_POST["direccion"],ENT_QUOTES)));//Escanpando caracteres 
 				
 				$error = array();
 				//Validaciones
 				if(!(strlen($cuit) == 11 && ctype_digit($cuit))){
-					$error[] = "Cuit debe tener 11 digitos sin guiones.";
+					$error[] = "El CUIT debe tener 11 dígitos sin guiones.";
 				  }
 				if(!(ctype_digit($codigoPostal) && strlen($codigoPostal) == 4)){
-					$error[] = "Codigo Postal debe tener 4 digitos.";
+					$error[] = "El Código Postal debe tener 4 dígitos.";
 				}
 				if(!(ctype_digit($telefono) && strlen($telefono) >= 8 && strlen($telefono) <= 11)){
-					$error[] = "Telefono debe tener entre 8 y 11 digitos.";
+					$error[] = "El Teléfono debe tener entre 8 y 11 dígitos.";
 				}
 				if(!(strlen($direccion) <= 70)){
-					$error[] = "Dirección debe tener maximo 70 caracteres.";
+					$error[] = "La Dirección debe tener como máximo 70 caracteres.";
 				}
 				$proveedor = new Proveedor();
 				$cuitValido = $proveedor::validarCuit($cuit);
 				if(!$cuitValido){
-					$error[] = "Cuit invalido.";
+					$error[] = "CUIT inválido.";
 				}
 
 				if(!(filter_var($correo, FILTER_VALIDATE_EMAIL))){
-					$error[] = "Email incorrecto";
+					$error[] = "Email incorrecto o inválido.";
 				}
-				//Validacion datos actuales
-				// $cek = mysqli_query($conexion, "SELECT * FROM consorcio WHERE idConsorcio='$nik'");
-				// if (mysqli_num_rows($cek) == 0) {
-				// 	// ERROR, no existe el consorcio!
-				// 	// Aca hay que abortar todo...
-				// } else {
-				// 	$row = mysqli_fetch_assoc($sql);
-				// }
 
 				$correoViejo = $row ['correo'];
 				$cuitViejo = $row ['cuit'];
 				
 				//Correo
-				$cek8 = mysqli_query($conexion, "SELECT * FROM consorcio WHERE correo = '$correo' AND idConsorcio <> '$nik'");
-				if (mysqli_num_rows($cek8) != 0){
-					$error[] = "Email está utilizado en otro consorcio.";
+				$queryCorreoRepetido = mysqli_query($conexion, "SELECT * FROM consorcio WHERE correo = '$correo' AND idConsorcio <> '$nik'");
+				if (mysqli_num_rows($queryCorreoRepetido) != 0){
+					$error[] = "El Email está dado de alta en otro consorcio.";
 				}
 
 				//Cuit
-				$cek4 = mysqli_query($conexion, "SELECT * FROM consorcio WHERE cuit = '$cuit' AND idConsorcio <> '$nik'");
-				if (mysqli_num_rows($cek4) != 0){
-					$error[] = "Cuit está utilizado en otro consorcio.";
+				$queryCuitRepetido = mysqli_query($conexion, "SELECT * FROM consorcio WHERE cuit = '$cuit' AND idConsorcio <> '$nik'");
+				if (mysqli_num_rows($queryCuitRepetido) != 0){
+					$error[] = "El CUIT ya está dado de alta en otro consorcio.";
 				}
 
                 if (sizeof($error) == 0){
                    $update = mysqli_query($conexion, "UPDATE consorcio SET nombre='$nombre', cuit='$cuit', codigoPostal='$codigoPostal', telefono='$telefono', correo='$correo', direccion='$direccion' WHERE idConsorcio='$nik'") or die(mysqli_error($conexion));
-                    if($update){
-                        //header("Location: editarConsorcio.php?nik=".$nik."&pesan=sukses");
+                    if ($update) {
+						// Busco nuevamente el consorcio para obtener los datos actualizados y mostrarlos en los controles
+						$sql = mysqli_query($conexion, "SELECT * FROM consorcio WHERE idConsorcio='$nik'");
+						$row = mysqli_fetch_assoc($sql);
                         echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Bien hecho! Los datos han sido modificados con éxito.</div>';
-                    }else{
-                        echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Error, no se pudo guardar los datos.</div>';
+                    } else {
+                        echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Error, no se pudieron actualizar los datos.</div>';
                     }
-                 }else{
-					echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Error. El CUIT del consorcio no es valido.</div>';
-					foreach($error as $er){
-						echo "</br><strong>$er</strong>";
-					  }
+                 } else {
+					echo '<div class="alert alert-danger alert-dismissable">
+							<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Errores:</strong>';
+							foreach ($error as $er) {
+								echo "<br>- $er";
+							  }
+						echo '</div>';
 					}
-					
-					if(isset($_GET['pesan']) == 'sukses'){
-						echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Los datos han sido guardados con éxito.</div>';
-				}
                 }
 
                  ?>
