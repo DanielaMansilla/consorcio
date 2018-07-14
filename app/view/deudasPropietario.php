3302aa9379b8db1559b72b48d0f762f810222d61
Federico Bevacqua <fede.el.xeneize@gmail.com>
2018-07-14 04:04:34 -0300

    Introduccion deudasPropietario. Agregadas 2 fotos.

--- app/view/deudasPropietario.php
+++ app/view/deudasPropietario.php
@@ -1,1 +1,33 @@
-/* Mostrar monto total de expensas impagas O mostrar lista de expensas pasadas de fecha*/
+<?php /* Mostrar monto total de expensas impagas O mostrar lista de expensas pasadas de fecha*/
+require_once '../config/Conexion.php'; 
+session_start();
+if (!isset($_SESSION['propietario'])) {
+    header("Location: index.php");
+} 
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<?php include('template/head.php'); ?>
+    <body>
+    <?php 
+        include('template/nav.php');  
+        include('template/header.php'); ?>
+    
+    <title>Consorcios del Valle - Deudas</title>
+
+
+
+
+
+
+
+
+
+
+
+    <div class="corte">
+    </div>
+        <?php include('template/footer.php'); ?>
+    </body>
+</html>
