a7475fabfcab0bfffab20faf643334a6e7e4bd84
Federico Bevacqua <fede.el.xeneize@gmail.com>
2018-07-14 06:03:19 -0300

    Actualizada la lista con mas datos y boton para ver los gastos e informe mensual de una liquidación.

--- app/view/listaLiquidacion.php
+++ app/view/listaLiquidacion.php
@@ -30,7 +30,9 @@ if(!isset($_SESSION['admin']) && !isset($_SESSION['operador'])) {
 					<th>Id</th>
 					<th>Año</th>
 					<th>Mes</th>
-					<th>Fecha</th>
+					<th>Dia</th>
+					<th>Fecha Completa</th>
+					<th>Informe Mensual</th>
                   
 				</tr>
 				<?php
@@ -46,36 +48,16 @@ if(!isset($_SESSION['admin']) && !isset($_SESSION['operador'])) {
 				} else {
 					while ($row = mysqli_fetch_assoc($sql)) {
 						echo '
-
 						<tr>
 							<td>'.$row['idLiquidacion'].'</td>
 							<td>'.date("Y", strtotime($row['periodo'])).'</td>
 							<td>'.date("F", strtotime($row['periodo'])).'</td>
+							<td>'.date("l", strtotime($row['periodo'])).'</td>
 							<td>'.$row['fecha'].'</td>
-							';
-                    
-                            //<a href="listaLiquidacion.php?nik='.$row['idLiquidacion'].'" title="Visualizar datos" class="btn btn-primary btn-sm"><span class="fas fa-search" aria-hidden="true"></span></a>
-
-							// if(isset($_SESSION['admin'])){
-							// 	echo '
-							// </td>
-							// <td>
-							// 	<a href="abm/editarUsuario.php?nik='.$row['idUsuarios'].'" title="Editar datos" class="btn btn-primary btn-sm"><span class="fas fa-edit" aria-hidden="true"></span></a>
-							// 	<a href="listaUsuario.php?aksi=delete&nik='.$row['idUsuarios'].'" title="Eliminar" onclick="return confirm(\'Esta seguro de borrar los datos de: '.$row['idReclamo'].' '.$row['idReclamo'].'?\')" class="btn btn-danger btn-sm"><span class="fas fa-trash" aria-hidden="true"></span></a>
-							// </td>
-							// </tr>
-							// ';
-							// }
-
-							// if (isset($_SESSION['operador'])) {
-							// 	echo '
-							// </td>
-							// <td>
-							// 	<a href="abm/editarRolyEstado.php?nik='.$row['idUsuarios'].'" title="Editar datos" class="btn btn-primary btn-sm"><span class="fas fa-edit" aria-hidden="true"></span></a>
-							// </td>
-							// </tr>
-							// ';
-							// }
+							<td>
+							 	<a href="informeMensual.php?year='.date("Y",strtotime($row['fecha'])).'&month='.date("m",strtotime($row['fecha'])).'" title="Ver Liquidación" class="btn btn-primary btn-sm"><span class="fas fa-list-ul" aria-hidden="true"></span></a>
+							</td>
+							 ';
 					}
 				}
 				?>
