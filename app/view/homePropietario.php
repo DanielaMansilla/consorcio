ac572ace395981f098fe5e358e10cb13e83dcbb4
Federico Bevacqua <fede.el.xeneize@gmail.com>
2018-07-13 21:54:46 -0300

    Actualizado el homePropietario, con sus respectivas imagenes y descripción.

--- app/view/homePropietario.php
+++ app/view/homePropietario.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../config/Conexion.php'; 
 session_start();
-if(!isset($_SESSION['propietario'])){  // verificar si es propietario
+if(!isset($_SESSION['propietario'])){
     header("Location: index.php");} ?>
 
 <!DOCTYPE html>
@@ -15,21 +15,15 @@ if(!isset($_SESSION['propietario'])){  // verificar si es propietario
     
     <title>Consorcios del Valle - Propietario</title>
 
-    <!-- Darle la bienvenida al usuario, mostrar con cards las disitntas opciones que puede realizar, Reclamos, informe menual, ver deudas, hacer reclamos.
-        con el titulo y su explicacion. Ej: Titulo: Informe Mensual. Descripcion: En esta seccion contara con un informe detallado sobre...
-
-        TODO: Cambiar que sean de 2 filas. 6+6 = 12
-     -->
-
   <div class="album py-5 bg-light">
         <div class="container">
 
           <div class="row">
             <div class="col-md-6">
               <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22208%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20208%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16487ba491b%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A11pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16487ba491b%22%3E%3Crect%20width%3D%22208%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2266.953125%22%20y%3D%22117.3%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+              <img class="card-img-top" src="../../public/img/expensas.jpg?text=Expensas">
                 <div class="card-body">
-                  <p class="card-text">Descripcion de expensas......</p>
+                  <p class="card-text">En esta sección encontraras todas las expensas de cada mes pagas e impagas de tu propiedad.</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                         <a href="listaExpensas.php" class="btn btn-primary">Ir a Expensas</a>
@@ -40,9 +34,9 @@ if(!isset($_SESSION['propietario'])){  // verificar si es propietario
             </div>
             <div class="col-md-6">
               <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22208%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20208%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16487ba491c%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A11pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16487ba491c%22%3E%3Crect%20width%3D%22208%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2266.953125%22%20y%3D%22117.3%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+              <img class="card-img-top" src="../../public/img/deudas.jpg?text=Deudas">
                 <div class="card-body">
-                  <p class="card-text">Descripcion de Deudas......</p>
+                  <p class="card-text">En esta sección encontraras el monto total que adeudas por tus propiedades.</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                         <a href="deudasPropietario.php" class="btn btn-primary">Ir a Deudas</a>
@@ -54,9 +48,9 @@ if(!isset($_SESSION['propietario'])){  // verificar si es propietario
 
             <div class="col-md-6">
               <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22208%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20208%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16487ba491c%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A11pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16487ba491c%22%3E%3Crect%20width%3D%22208%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2266.953125%22%20y%3D%22117.3%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+              <img class="card-img-top" src="../../public/img/informeMensual.jpg?text=InformeMensual">
                 <div class="card-body">
-                  <p class="card-text">Descripcion de Informe Mensual......</p>
+                  <p class="card-text">Aquí podrás encontrar una minuta detallada de la liquidación mensual integrada por gastos realizados por la administración y pagos de cada propietario con un resumen final.</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                         <a href="informePropietario.php" class="btn btn-primary">Ir a Informe Mensual</a>
@@ -67,9 +61,9 @@ if(!isset($_SESSION['propietario'])){  // verificar si es propietario
             </div>
             <div class="col-md-6">
               <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22208%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20208%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16487ba491d%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A11pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16487ba491d%22%3E%3Crect%20width%3D%22208%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2266.953125%22%20y%3D%22117.3%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+              <img class="card-img-top" src="../../public/img/reclamos.jpg?text=Reclamos">
                 <div class="card-body">
-                  <p class="card-text">Descripcion de Reclamos.....</p>
+                  <p class="card-text">Aquí podrás contactarte con nuestros administradores sobre algún problema que tenga tu propiedad o el edificio, para poder solucionarlo lo más rápido y efectivo posible.</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                         <a href="listaReclamos.php" class="btn btn-primary">Ir a Reclamos</a>
